13ea9db341e146b54f5332c0a732dd418244a1ff
puczilka <marta.monika.kwiatkowska@gmail.com>
2020-04-29 22:31:40 +0100

    heroku struggle

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ import plotly.graph_objs as go
 import flask
 from dash.dependencies import Input, Output
 
-server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
+# server = flask.Flask(__name__)
+# server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+# app = dash.Dash(__name__, server=server)
+
+app = dash.Dash(__name__)
+server = app.server
 
 print(dcc.__version__) # 0.6.0 or above is required
 
